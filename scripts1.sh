e77fa37556d24dea0b7e710d6b393e8627e83f38
shivajajula06 <shivajajula06@gmail.com>
2023-05-16 01:39:19 +0530

    scripts

---
+++ scripts1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#########echo#######
+echo ----------------
+echo "first script"
+echo ----------------
+date
+echo ----------------
+uname -a
+echo ----------------
+uptime
+echo ----------------
